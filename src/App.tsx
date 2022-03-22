e6331881ff326b19a68b3f97f801f6ed1ddfd1cf
Aline Avelino <aline.savelino@gmail.com>
2022-03-22 18:46:38 -0300

    Renderização e estilo header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Header } from "./components/Header";
+
 export function App() {
   return (
-    <h1>To-Do</h1>
+    <Header />
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,13 @@
+import styles from './styles.module.scss';
+import moon from '../../img/icon-moon.svg';
+
 export function Header() {
     return (
-        <h2>Ok</h2>
+        <main className={styles.backgroundHeader}>
+            <div className={styles.headerContainer}>
+                <h1>TODO</h1>
+                <img src={moon} alt="Lua para trocar de tema" />
+            </div>
+        </main>
     )
 }
