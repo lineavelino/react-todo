be5a8953f40046e0b99b04dfcb4779ed88e4c66a
Aline Avelino <aline.savelino@gmail.com>
2022-03-23 18:55:16 -0300

    Renderizar task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { InputTask } from './components/InputTask';
 import { Header } from './components/Header';
+import { TaskList } from './components/TaskList';
+import styles from './App.module.scss';
 
 export function App() {
   return (
-    <main>
+    <>
       <Header />
-      <InputTask />
-    </main>
-   )
+      <main className={styles.container}>
+        <InputTask />
+        <TaskList />
+      </main>
+    </>
+  )
 }
